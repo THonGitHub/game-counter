98cafc18569ff45e1cd9305704b06b8fb904d06c
Timotej Horňák <timotej.hornak@student.tuke.sk>
2021-12-19 21:14:08 +0100

    layout update

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,16 @@
 import React from "react";
 import "./App.css";
 import Header from "./layout/Header";
+import Main from "./layout/Main";
+import Footer from "./layout/Footer";
+import styles from "./App.module.css";
 
 function App() {
   return (
-    <div className="App">
+    <div className={styles.wrap}>
       <Header />
+      <Main />
+      <Footer />
     </div>
   );
 }
